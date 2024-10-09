b8131e0aadc51795bf4e0478b3aca757023b9e93
Ilya Shchelkin <ilyashchelkin@mail.ru>
2024-10-09 14:56:21 +0400

    Внёс небольшие правки для работы с Discord

--- zapret-winws/service_create.cmd
+++ zapret-winws/service_create.cmd
@@ -1,7 +1,14 @@
-set ARGS=--wf-l3=ipv4,ipv6 --wf-tcp=80,443 --dpi-desync=fake,split --dpi-desync-ttl=7 --dpi-desync-fooling=md5sig
+set ARGS=^
+--wf-tcp=80,443 ^
+--wf-udp=443,50000-65535 ^
+--filter-udp=443 --hostlist=\"%~dp0list-youtube.txt\" --dpi-desync=fake,tamper --dpi-desync-repeats=11 --dpi-desync-fake-quic=\"%~dp0quic_initial_www_google_com.bin\" --new ^
+--filter-udp=443 --dpi-desync=fake,tamper --dpi-desync-autottl=2 --dpi-desync-repeats=11 --new ^
+--filter-udp=50000-65535 --dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-autottl=2 --dpi-desync-repeats=11 --new ^
+--filter-tcp=80 --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --new ^
+--filter-tcp=443 --hostlist=\"%~dp0list-youtube.txt\" --dpi-desync=fake,disorder2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=\"%~dp0tls_clienthello_www_google_com.bin\" --new ^
+--dpi-desync=fake,tamper --dpi-desync-any-protocol --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=\"%~dp0tls_clienthello_www_google_com.bin\"
+
 call :srvinst winws1
-rem set ARGS=--wf-l3=ipv4,ipv6 --wf-udp=443 --dpi-desync=fake 
-rem call :srvinst winws2
 goto :eof
 
 :srvinst
